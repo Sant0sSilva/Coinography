020b1a499eeb970c8d8c5cd0e9be5757bbf75739
Sant0sSilva <lourencos88@gmail.com>
2025-01-08 19:24:20 +0000

    coin card layout update

--- lib/coin_card.dart
+++ lib/coin_card.dart
@@ -1,10 +1,7 @@
-import 'package:coinography/coins.dart';
 import 'package:flutter/material.dart';
 import 'coin.dart';
-import 'coin.dart';
-import 'coins_list.dart';
 
-// coinCard must display coinTitle, coinPrice, coin24h, holdings
+// coinCard must display coinTitle, coinPrice, coin24h, holdings. profit/loss
 
 class CoinItem extends StatelessWidget {
   const CoinItem(this.coin, {super.key});
@@ -13,31 +10,50 @@ class CoinItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 20,
-        ),
-        child: Column(
-          children: [
-            Text(coin.coinTitle),
-            const SizedBox(height: 4,),
-            Row(
-              children: [
-                Text('\$${coin.amountUSD.toStringAsFixed(2)}',),
-                const Spacer(),
-                Row(
-                  children: [
-                    Text('\$${coin.tokenAmount.toStringAsFixed(2)}  ',),
-                    // const Spacer(),
-                    // Text('${coin.coin24H.toStringAsFixed(2)}%'),
-
-                  ],
-                )
-              ],
-            )
-          ],
+    return InkWell(
+      onTap: () {
+        print('tap tap');
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 20,
+          ),
+          child: Column(
+            children: [
+              Text(coin.coinTitle),
+              const SizedBox(
+                height: 4,
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Holding: \$${coin.amountUSD.toStringAsFixed(2)}',
+                  ),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      Text(
+                        'price: \$${coin.tokenAmount.toStringAsFixed(2)}  ',
+                      ),
+                      // const Spacer(),
+                      // Text('${coin.coin24H.toStringAsFixed(2)}%'),
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Text('P/L'),
+                  const Spacer(),
+                  Row(children: [
+                    Text('24h'),
+                  ],)
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
